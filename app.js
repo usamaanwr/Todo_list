a3aaee49c3bdaebec68ae8389570eb1793561f2c
Muhammad Osama <anweru98@gmail.com>
2025-08-16 01:23:53 +0500

    replace files

--- app.js
+++ app.js
@@ -1,69 +1,130 @@
-// var inputbox = document.getElementById("input_box");
-// var listcontainer = document.getElementById("list_container");
+// // const inputs = document.getElementById("todo_inputs");
+// const list = document.getElementById("list_container")
+// let todo_list = [];
+// let todo_count = 0;
 
-// function addTask() {
-//     if (inputbox.value === "") {
-//         alert("You must write something");
-//     } else {
-//         var li = document.createElement("li");
-//         li.innerHTML = inputbox.value;
-//         listcontainer.appendChild(li);
-
-//         var span = document.createElement("span");
-//         span.innerHTML = "\u00d7";
-//         li.appendChild(span);
+// function show_task() {
+//     if (inputs.value.trim() !== "") {
+//         todo_list.push({task: inputs.value, id: todo_count++})
+//         // todo_count++;
 //     }
-//     inputbox.value = "";
-//     saveData();
+//     display();
+//     inputs.value = "";
+//     console.log(todo_list);
+// }
+// function display(id) {
+//    let html = '';
+
+// for (let i = 0; i < todo_list.length; i++) {
+//     html +=`<li class="todo_item">${todo_list[i].task}
+//     <div id="edit_todo${todo_list[i].id}"></div>
+//     <div class="icon">
+//      <span onclick="edit(${todo_list[i].id})"><i class="fa-solid fa-pen-to-square"></i></span>
+//      <span onclick="delet(${todo_list[i].id})"><i class="fa-solid fa-trash"></i></span>
+//     </div>
+//     </li>`
+// }
+// list.innerHTML = html;
 // }
 
-// listcontainer.addEventListener("click", function (e) {
-//     if (e.target.tagName === "LI") {
-//         e.target.classList.toggle("checked"); // fixed typo here
-//         saveData();
-//     } else if (e.target.tagName === "SPAN") {
-//         e.target.parentElement.remove(); // fixed typo here
-//         saveData();
+// function delet(id) {
+//     for (let i = 0; i < todo_list.length; i++) {
+//       if (todo_list[i].id === id) {
+//         todo_list.splice(i ,1)
+//       }
 //     }
-// }, false);
+//     display()
+// }
+// let eidt_enabled = 0;
+// function edit(id) {
+//   if (eidt_enabled === 0) {
+//     eidt_enabled = 1
+//     let edit_div = document.getElementById("edit_todo"+id)
 
-// function saveData() {
-//     localStorage.setItem("data", listcontainer.innerHTML);
+
+// edit_div.innerHTML = `<input type="text" id="todoupdate${id}">
+// <span onclick="updatetask(${id})"><i class="fa-solid fa-arrow-rotate-right"></i></span>
+// `
+//   }
 // }
 
-// function showTask() {
-//     listcontainer.innerHTML = localStorage.getItem("data");
+// function updatetask(id) {
+//    edit_div = document.getElementById("edit_todo"+id);
+//   let edit_inputs = document.getElementById("todoupdate" + id);
+// console.log(edit_inputs.value)
+//  for (let i = 0; i < todo_list.length; i++) {
+//       if (todo_list[i].id === id) {
+//         todo_list.splice(i ,1 ,{task: edit_inputs.value, id: id})
+//       }
+//     }
+//     edit_div.innerHTML = '';
+//     display()
+//     eidt_enabled = 0
 // }
 
-// showTask();
+const inputs = document.getElementById("todo_inputs");
+const list =document.getElementById("list_container");
+let todo_push = [];
+let todo_count = 0;
 
-function addTask() {
-    var input = document.getElementById("input_box");
-    var task = input.value;
+function show_task() {
+  if (inputs.value.trim() !== "") {
+    todo_push.push({task: inputs.value , id : todo_count++})
+  }
+  display_list()
+  inputs.value= "";
+  console.log(todo_push);
+}
 
-    if (task !== "") {
-        var li = document.createElement("li");
-        li.innerHTML = task;
+function display_list(id) {
+  let html = '';
+  for (let i = 0; i< todo_push.length; i++){
 
-        // Append li to the list
-        document.getElementById("list_container").appendChild(li);
+html += `<li class = "todo_item">${todo_push[i].task}
+<div id ="edit_todo${todo_push[i].id}"></div>
+<div class = "icon">
+   <span onclick="edit(${todo_push[i].id})"><i class="fa-solid fa-pen-to-square"></i></span>
+    <span onclick="delet(${todo_push[i].id})"><i class="fa-solid fa-trash"></i></span>
+    </div>
+</li>`
+  }; 
+  list.innerHTML = html;
+}
+ 
+function delet(id) {
+  for (let i = 0; i < todo_push.length; i++) {
+  if (todo_push[i].id === id) {
+    todo_push.splice(i , 1);
+  }
+  }
+  display_list()
+};
 
-        // Create span for delete
-        var span = document.createElement("span");
-        span.innerHTML = "\u00d7"; // Unicode for ×
-        li.appendChild(span);
+let eidt_enabled = 0
+function edit(id) {
+  if (eidt_enabled === 0) {
+    eidt_enabled = 1
+    let edit_div = document.getElementById("edit_todo"+id);
 
-        // Correct delete functionality
-        span.onclick = function() {
-            li.remove();
-        };
+    edit_div.innerHTML = `<input type="text" id="todoupdate${id}">
+ <span onclick="updatetask(${id})"><i class="fa-solid fa-arrow-rotate-right"></i></span>
+`
+  }
+}
 
-        // Clear input
-        input.value = "";
+function updatetask(id) {
+    edit_div = document.getElementById("edit_todo" + id);
+  let edit_todo = document.getElementById("todoupdate" + id)
+  
+  
+  for (let i = 0; i < todo_push.length; i++) {
+    if (todo_push[i].id === id) {
+      
+      todo_push.splice(i ,1 , {task :edit_todo.value , id: id})
     }
-    else{
-        if (task=== "") {
-            alert("You must write something")
-        }
-    }
+    
+  }
+  edit_div.innerHTML = "";
+  display_list()
+  eidt_enabled = 0;
 }
